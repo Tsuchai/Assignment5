9a91faebbdd3a5383213bda70a5975d474544479
tsuchai <tecsonethan@gmail.com>
2024-03-22 21:21:06 -0700

    Final ver.

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,8 +4,32 @@ function biggerClick(){
 
 function changeStyle() {
     let radioButton = document.getElementById("fancyRadio")
-    if (radioButton.checked)
-        document.getElementById("textInput").style.fontWeight="bold"
-    else
-        document.getElementById("textInput").style.fontWeight="normal"
+    if (radioButton.checked) {
+        let text = document.getElementById("textInput")
+        text.style.fontWeight = "bold"
+        text.style.textDecoration = "underline"
+        text.style.color = "blue"
+    } else {
+        let text = document.getElementById("textInput")
+        text.style.fontWeight = "normal"
+        text.style.textDecoration = "none"
+        text.style.color = "black"
+    }
+}
+
+function mooClick() {
+    let text = document.getElementById("textInput").value;
+    let sentences = text.split('.');
+    for (let i = 0; i < sentences.length; i++)
+    {
+        let sentence = sentences[i].trim()
+        let words = sentence.split(' ')
+        let lastWordIndex = words.length - 1
+        let lastWord = words[lastWordIndex];
+        if (lastWord.trim() !== "") {
+            words[lastWordIndex] = lastWord + "-moo";
+        }
+        sentences[i] = words.join(' ');
+    }
+    document.getElementById("textInput").value = sentences.join('. ');
 }
